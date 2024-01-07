64b5765e9e437e19b0112197f96db0172aed6b46
AtraeNex <vvasilasko@gmail.com>
2024-01-07 16:42:30 +0200

    Complete code re-write

---
+++ README.md
@@ -0,0 +1,46 @@
+# Data Crawler
+
+## Description
+
+This data crawler 
+
+## Installation
+
+### Using a package manager
+
+You can install the crawler as a package: Using `pip`:
+
+```sh
+pip install Slavka_data_crawler
+```
+
+Or using `poetry`:
+
+```sh
+poetry add Slavka_data_crawler
+```
+
+### Cloning the repository
+
+You can also clone the repository and install the dependencies. Using `poetry`:
+
+```sh
+git clone https://github.com/AtraeNex/Slavka_data_crawler
+cd data_crawler_examples
+poetry install
+```
+
+## Usage
+
+### As a module
+
+```python
+#from Slavka_data_crawler import crawl
+
+#print(crawl("lrytas", "df", query="vakcinacija", date_from="2023-01-01", time_limit=10))
+```
+
+## Structure
+
+The project is structured as follows:
+
--- Slavka_data_crawler/main.py
+++ Slavka_data_crawler/main.py
@@ -1,62 +1,29 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from queue import Queue
-from lxml.etree import HTML
-from requests import get
-from threading import Lock
-
-# Global variables
-visited_pages = set()
-visited_articles = set()
-pages_queue = Queue()
-articles_queue = Queue()
-
-lock = Lock()
-
-def query_to_url(query: str) -> str:
-    return f"https://www.lrytas.lt/search?q={query}"
+import httpx
+from selectolax.parser import HTMLParser
 
 
-def process_page(page_url: str):
-    with lock:
-        print(page_url)
-    # global visited_pages, visited_articles, pages_queue, articles_queue
+url = "https://www.rei.com/c/camping-and-hiking"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"}
+
+response = httpx.get(url, headers=headers)
+html = HTMLParser(response.text)
+
+#Sukuriam funkcija, kur grazins "None", jeigu neras "Sale-Price"
+#kadangi nevisos kainos surasytos su nuolaida. Reikia ismesti klaida, kai kaina randama be nuolaidos
+def extract_text(html, selector):
     try:
-        response = get(page_url)
-        tree = HTML(response.text)
-        for page_link in tree.xpath(
-            "//a[contains(@class, 'LPagination__button')]/@href"
-        ):
-            full_page_url = "https://www.lrytas.lt" + page_link
-            if full_page_url not in visited_pages:
-                pages_queue.put(full_page_url)
-                visited_pages.add(full_page_url)
-        for article_link in tree.xpath("//a[@class='LPostContent__anchor']/@href"):
-            article_url = "https://www.lrytas.lt" + article_link
-            if article_url not in visited_articles:
-                visited_articles.add(article_url)
-                articles_queue.put(article_url)
-    except Exception as e:
-        print("Error retrieving url:", e)
+        return html.css_first(selector).text()
+    except AttributeError:  #nereikia 'as err', nes returninam None
+        return None
 
+#Pasirenkam li.VcGDfKKy_dvNbxUqm29K, kadangi
+products = html.css("li.VcGDfKKy_dvNbxUqm29K")
 
-def main():
-    initial_url = query_to_url("rytas")
-    pages_queue.put(initial_url)
-    visited_pages.add(initial_url)
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = set()
-        while not pages_queue.empty() or futures:
-            # Submit new tasks from the queue
-            while not pages_queue.empty() and len(futures) < 10:
-                page = pages_queue.get()
-                future = executor.submit(process_page, page)
-                futures.add(future)
-            # Wait for the next future to complete
-            done = as_completed(futures, timeout=None)
-            for future in done:
-                futures.remove(future)
-    print("Finished processing all pages.")
-
-
-if __name__ == "__main__":
-    main()
+#Istraukiam visus produktus is vieno/pasirinkto puslapio
+for product in products:
+    #dictionary informacijai atvaizduoti
+    item = {
+        "name": extract_text(product, ".Xpx0MUGhB7jSm5UvK2EY"),
+        "price": extract_text(product, "span[data-ui=sale-price]")
+    }
+    print(item)
